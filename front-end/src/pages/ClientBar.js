70d61efd3b8d29a46acbbd1ef86d7cc308c46d8b
adampalharine <adampalharine@gmail.com>
2022-10-28 19:12:18 -0300

    feat - construção da tabela de produtos

--- front-end/src/pages/ClientBar.js
+++ front-end/src/pages/ClientBar.js
@@ -7,25 +7,56 @@ class Client extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      products: [], 
+    };
   }
 
   async componentDidMount() {
     const products = await requestData('/products');
-    console.log(products);
+    // console.log(products);
+    this.setState({
+      products: [...products]
+    })
   }
 
   render() {
+    const { products } = this.state;
     return (
       <div>
         <NavBar />
         <div>
           <ul>
-            {/* { products.map(() => {
-              <li>
-                <produtos />
-              </li>;
-            }) } */}
+            { products.map((products, index) => {
+              index !== 0 && (
+                <section>
+                  <li
+                  data-testid={`customer_products__element-card-price-${products.id}`}
+                  key={ index }>
+                    <> { products.url_image } </>
+                    <> { products.name } </>
+                    <> { products.price } </>
+                    <button
+                    data-testid="button_add"
+                    onChange={ this.handleChange }
+                    > Adicionar ao Carrinho 
+                    </button>
+                    <input 
+                      data-testid="button_remove"
+                      placeholder="0"
+                      type="text"
+                      name="text"
+                      onChange={ this.handleChange }
+                    />
+                    <button
+                    data-testid=""
+                    onChange={ this.handleChange }
+                    > Remover do Carrinho 
+                    </button>
+                  </li>
+                </section>
+                )
+            }) }
           </ul>
         </div>
       </div>
