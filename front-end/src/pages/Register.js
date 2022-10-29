8457167c57f2037069dc96f4eecc93dd1f17952e
VanehSann <vanessacbsantos22@gmail.com>
2022-10-29 13:26:39 -0300

    feat: add some changes to /register

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,12 +1,12 @@
+import propTypes from 'prop-types';
 import React, { Component } from 'react';
-import { useHistory } from 'react-router-dom';
 
 class Register extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: '',
+      username: '',
       email: '',
       password: '',
       isDisabled: true,
@@ -17,15 +17,16 @@ class Register extends Component {
   handleChange = ({ target }) => {
     const PASSWORD_MAX_LENGTH = 6;
     const NAME_MIN_LENGTH = 12;
+
     const { name, value } = target;
 
     this.setState({
       [name]: value,
     }, () => {
-      const { email, password } = this.state;
+      const { username, email, password } = this.state;
       if (this.validateEmail(email)
        && password.length >= PASSWORD_MAX_LENGTH
-       && name.length >= NAME_MIN_LENGTH) {
+       && username.length >= NAME_MIN_LENGTH) {
         this.setState({
           isDisabled: false,
           errorHandling: false,
@@ -45,66 +46,69 @@ class Register extends Component {
   };
 
   redirectToProducts = () => {
-    const history = useHistory();
+    const { history } = this.props;
     history.push('/customer/products');
+    console.log('cheguei aqui');
   };
 
   render() {
-    const { email, password, isDisabled, errorHandling, name } = this.state;
+    const { email, password, isDisabled, errorHandling, username } = this.state;
 
     return (
-      <fieldset>
-        <p>Cadastro</p>
-        <form>
-          <label htmlFor="input-name">
-            <input
-              id="input-name"
-              data-testid="common_register__input-name"
-              placeholder="Insira seu nome"
-              type="name"
-              name="name"
-              value={ name }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="input-email">
-            <input
-              id="input-email"
-              data-testid="common_register__input-email"
-              placeholder="Insira seu e-mail"
-              type="email"
-              name="email"
-              value={ email }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <label htmlFor="input-password">
-            <input
-              id="input-password"
-              data-testid="common_register__input-password"
-              placeholder="Insira sua senha"
-              type="password"
-              name="password"
-              value={ password }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            data-testid="common_register__button-register"
-            type="submit"
-            disabled={ isDisabled }
-            onClick={ () => this.redirectToProducts }
-          >
-            Cadastrar
-          </button>
-        </form>
-        { errorHandling && (
-          <span data-testid="common_register__element-invalid_register">
-            DADOS INVÁLIDOS PARA CADASTRO
-          </span>
-        ) }
-
-      </fieldset>
+      <>
+        <fieldset>
+          <p>Cadastro</p>
+          <form>
+            <label htmlFor="input-name">
+              <input
+                id="input-name"
+                data-testid="common_register__input-name"
+                placeholder="Insira seu nome"
+                type="text"
+                name="username" // fix : Que nome eu coloco aqui?
+                value={ username }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="input-email">
+              <input
+                id="input-email"
+                data-testid="common_register__input-email"
+                placeholder="Insira seu e-mail"
+                type="email"
+                name="email"
+                value={ email }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="input-password">
+              <input
+                id="input-password"
+                data-testid="common_register__input-password"
+                placeholder="Insira sua senha"
+                type="password"
+                name="password"
+                value={ password }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              data-testid="common_register__button-register"
+              type="button"
+              disabled={ isDisabled }
+              onClick={ this.redirectToProducts }
+            >
+              Cadastrar
+            </button>
+          </form>
+          { errorHandling && (
+            <span data-testid="common_register__element-invalid_register">
+              DADOS INVÁLIDOS PARA CADASTRO
+            </span>
+          ) }
+        </fieldset>
+        <p>teste</p>
+      </>
     );
   }
 }
@@ -113,12 +117,12 @@ class Register extends Component {
 //   dispatchRegisterChange: (email, role) => dispatch(userLogin(email, role)),
 // });
 
-// Resgister.propTypes = {
-//   history: propTypes.shape({
-//     push: propTypes.func.isRequired,
-//   }).isRequired,
-//   dispatchRegisterChange: propTypes.func.isRequired,
-// };
+Register.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
+  //  dispatchRegisterChange: propTypes.func.isRequired,
+};
 
 //  export default connect(null, mapDispatchToProps)(Register);
 export default Register;
